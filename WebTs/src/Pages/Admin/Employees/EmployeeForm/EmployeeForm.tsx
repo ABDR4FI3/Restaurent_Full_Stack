4060811ccfd8cce140adfe247b6b655722b92ed4
ABDR4FI3 <its.abdrafi3@gmail.com>
2024-08-10 11:33:11 +0100

    Restructuring Employee + DataBase File 	+ Making Departement separate enitity for better Scalability and control ✅ 	+ making position separate entity ...✅ 	+ adding real Data and exported DataBase

--- WebTs/src/Pages/Admin/Employees/EmployeeForm/EmployeeForm.tsx
+++ WebTs/src/Pages/Admin/Employees/EmployeeForm/EmployeeForm.tsx
@@ -7,11 +7,8 @@ interface EmployeeFormProps {
   action: "details" | "edit" | "add";
 }
 
-const EmployeeForm: React.FC<EmployeeFormProps> = ({ user }) => {
-  // Initialize formState with the user prop
+const EmployeeForm: React.FC<EmployeeFormProps> = ({ user, action }) => {
   const [formState, setFormState] = useState<Employee>({ ...user });
-
-  // Handler to update formState on input change
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormState((prevState) => ({
@@ -19,15 +16,20 @@ const EmployeeForm: React.FC<EmployeeFormProps> = ({ user }) => {
       [name]: value,
     }));
   };
+  const isDisabled = action === "details";
+  const handleAction = () => {
+    console.log("Action handled" + action);
+    console.log("Form state:", formState);
+  }
 
   return (
-    <div className="flex flex-col p-10 gap-4">
-      <h2 className="text-3xl">Employee Details :</h2>
+    <div className="flex flex-col p-10 gap-4 font-montserrat">
+      <h2 className="text-3xl">Employee Card :</h2>
       {/* Card + details */}
       <section className="flex gap-10">
         {/* Card */}
         <div
-          className="flex flex-col border border-black rounded-2xl p-4 gap-4 justify-center items-center"
+          className="flex flex-col border border-black rounded-2xl h-3/4 p-4 gap-4 justify-center items-center basis-1/3"
           style={{
             background: "rgba(173, 216, 230, 0.3)",
             backdropFilter: "blur(50px)",
@@ -42,102 +44,121 @@ const EmployeeForm: React.FC<EmployeeFormProps> = ({ user }) => {
           <h2>{formState.name}</h2>
           <p>{formState.position}</p>
         </div>
-        <div className="grid grid-cols-2 gap-4 basis-2/3">
-          {/* Input Fields Address */}
-          <div className="w-full flex-col flex">
-            <label htmlFor="address">Address</label>
-            <input
-              type="text"
-              name="address"
-              id="address"
-              value={formState.address}
-              onChange={handleChange}
-              className="border-2 border-black rounded-xl py-1 px-2"
-            />
+        <div className="flex flex-col basis-2/3">
+          <div className="grid grid-cols-2 gap-4 ">
+            {/* Input Fields Address */}
+            <div className="w-full flex-col flex">
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                name="address"
+                id="address"
+                value={formState.address}
+                onChange={handleChange}
+                disabled={isDisabled}
+                className="border-2 border-black rounded-lg py-1 px-2"
+              />
+            </div>
+            {/* Input Fields Email */}
+            <div className="w-full flex-col flex">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={formState.email}
+                onChange={handleChange}
+                disabled={isDisabled}
+                className="border-2 border-black rounded-lg py-1 px-2"
+              />
+            </div>
+            {/* Input Fields Department */}
+            <div className="w-full flex-col flex">
+              <label htmlFor="department">Department</label>
+              <input
+                type="text"
+                name="department"
+                id="department"
+                value={formState.department}
+                onChange={handleChange}
+                disabled={isDisabled}
+                className="border-2 border-black rounded-lg py-1 px-2"
+              />
+            </div>
+            {/* Input Fields Phone */}
+            <div className="w-full flex-col flex">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                value={formState.phone}
+                onChange={handleChange}
+                disabled={isDisabled}
+                className="border-2 border-black rounded-lg py-1 px-2"
+              />
+            </div>
+            {/* Input Fields Salary */}
+            <div className="w-full flex-col flex">
+              <label htmlFor="salary">Salary</label>
+              <input
+                type="text"
+                name="salary"
+                id="salary"
+                value={formState.salary}
+                onChange={handleChange}
+                disabled={isDisabled}
+                className="border-2 border-black rounded-lg py-1 px-2"
+              />
+            </div>
+            {/* Input Fields Shift */}
+            <div className="w-full flex-col flex">
+              <label htmlFor="shift">Shift</label>
+              <input
+                type="text"
+                name="shift"
+                id="shift"
+                value={formState.shift}
+                onChange={handleChange}
+                disabled={isDisabled}
+                className="border-2 border-black rounded-lg py-1 px-2"
+              />
+            </div>
+            {/* Input Fields Image */}
+            <div className="w-full flex-col flex">
+              <label htmlFor="image">Image</label>
+              <input
+                type="text"
+                name="image"
+                id="image"
+                value={formState.image}
+                onChange={handleChange}
+                disabled={isDisabled}
+                className="border-2 border-black rounded-lg py-1 px-2"
+              />
+            </div>
+            {/* Input Fields Hiring Date */}
+            <div className="w-full flex-col flex">
+              <label htmlFor="hiringDate">Hiring Date</label>
+              <input
+                type="date"
+                name="hiringDate"
+                id="hiringDate"
+                value={formatDate(formState.hiringDate + "")}
+                disabled
+                className="border-2 border-black rounded-lg py-1 px-2"
+              />
+            </div>
           </div>
-          {/* Input Fields Email */}
-          <div className="w-full flex-col flex">
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              value={formState.email}
-              onChange={handleChange}
-              className="border-2 border-black rounded-xl py-1 px-2"
-            />
-          </div>
-          {/* Input Fields Department */}
-          <div className="w-full flex-col flex">
-            <label htmlFor="department">Department</label>
-            <input
-              type="text"
-              name="department"
-              id="department"
-              value={formState.department}
-              onChange={handleChange}
-              className="border-2 border-black rounded-xl py-1 px-2"
-            />
-          </div>
-          {/* Input Fields Phone */}
-          <div className="w-full flex-col flex">
-            <label htmlFor="phone">Phone</label>
-            <input
-              type="text"
-              name="phone"
-              id="phone"
-              value={formState.phone}
-              onChange={handleChange}
-              className="border-2 border-black rounded-xl py-1 px-2"
-            />
-          </div>
-          {/* Input Fields Salary */}
-          <div className="w-full flex-col flex">
-            <label htmlFor="salary">Salary</label>
-            <input
-              type="text"
-              name="salary"
-              id="salary"
-              value={formState.salary}
-              onChange={handleChange}
-              className="border-2 border-black rounded-xl py-1 px-2"
-            />
-          </div>
-          {/* Input Fields Shift */}
-          <div className="w-full flex-col flex">
-            <label htmlFor="shift">Shift</label>
-            <input
-              type="text"
-              name="shift"
-              id="shift"
-              value={formState.shift}
-              onChange={handleChange}
-              className="border-2 border-black rounded-xl py-1 px-2"
-            />
-          </div>
-          {/* Input Fields Image */}
-          <div className="w-full flex-col flex">
-            <label htmlFor="image">Image</label>
-            <input
-              type="text"
-              name="image"
-              id="image"
-              value={formState.image}
-              onChange={handleChange}
-              className="border-2 border-black rounded-xl py-1 px-2"
-            />
-          </div>
-          {/* Input Fields Hiring Date */}
-          <div className="w-full flex-col flex">
-            <label htmlFor="hiringDate">Hiring Date</label>
-            <input
-              type="date"
-              name="hiringDate"
-              id="hiringDate"
-              value={formatDate(formState.hiringDate+"")}
-              disabled
-              className="border-2 border-black rounded-xl py-1 px-2"
-            />
+          <div
+            className={`flex my-4 justify-end ${
+              action === "details" ? "hidden" : ""
+            }`}
+          >
+            <button className="bg-green-500 hover:bg-transparent hover:text-green-500 hover:border-green-500 border duration-700 text-white font-bold py-3 px-5 rounded-lg"
+            onClick={handleAction}>
+              {action}
+            </button>
           </div>
         </div>
       </section>
